fca78df76f09bda4abdeadc97f4ff98346fd2288
SQ <blinkstarsq@gmail.com>
2026-03-13 21:00:53 +0800

    第二次push

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include "../include/math_utils.h"
-#include "math_utils.c"
 
 int main(void){
 	int n;
 	scanf("%d", &n);
-	printf("%d", factorial(n));
+	printf("%d\n", factorial(n));
 	return 0;
 }
